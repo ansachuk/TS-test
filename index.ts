ecb7e6ef8bc10f7f13dfa84fc806d3424643b77e
ansachuk <ansachuk@icloud.com>
2023-08-17 13:12:55 +0300

    basic and advanced types

--- index.ts
+++ index.ts
@@ -1,3 +1,41 @@
-const one: number = 1;
-const str = "Hello world!";
-console.log(str);
+// ? Basic types
+
+const numb: number = 1;
+const str: string = "Hello world!";
+const trueValue: boolean = true;
+
+let list: Array<number>;
+
+list = [1, 2, 3];
+
+// ? Advanced types
+
+//* enum
+enum Numbers {
+	zero,
+	one,
+	two,
+	three,
+}
+
+console.log(Numbers);
+
+//* never
+const err = (msg: string): never => {
+	throw new Error(msg);
+};
+
+//! err(str);
+
+//* object
+
+const create = (o: object | null): void => {};
+
+create(null);
+
+//* type
+
+type Name = string;
+
+const username: Name = "ansachuk";
+console.log(username);
